f7f6358309db21e22350798912cf9f89f6a5fb37
Leandro <leandro.ln.dev@gmail.com>
2022-06-20 00:26:54 -0300

    remove comments

--- js/app.js
+++ js/app.js
@@ -8,14 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-// usar async await para que se espere a adquirir los datos para poder ejecutarse el programa
 const fetchData = async (id) => {
     try {
-        // llamar a la api
         const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        // convertir a json requerido al utilizar fetch
         const data = await result.json();
-        console.log(data);
 
         const pokemon = {
             img: data.sprites.other.dream_world.front_default,
@@ -33,18 +29,12 @@ const fetchData = async (id) => {
     }
 }
 
-const paintCard = (pokemon) =>{
-    console.log(pokemon);
-    // donde va a ir el template
+const paintCard = (pokemon) => {
     const flex = document.getElementById('flex');
-    // capturar el template
     const template = document.getElementById('template-card').content
-    // recomendable hacer un clone del template
     const clone = template.cloneNode(true);
-    // utilizar siempre el fragment en un template
     const fragment = document.createDocumentFragment();
 
-    
     clone.querySelector('.card-body-img').setAttribute('src', pokemon.img);
     clone.querySelector('.card-body-title').innerHTML = `${pokemon.name} <span>${pokemon.hp} hp</span>`
     clone.querySelector('.card-body-text').textContent = `${pokemon.experience} Exp`;
@@ -52,12 +42,10 @@ const paintCard = (pokemon) =>{
     clone.querySelectorAll('.card-footer-statistics h3')[1].textContent = `${pokemon.special} K`;
     clone.querySelectorAll('.card-footer-statistics h3')[2].textContent = `${pokemon.defense} K`;
 
-    // hay que pasar nuestro clon al fragment
     fragment.appendChild(clone);
-    // una ves que esta en el fragment lo pasamos al flex-container
     flex.appendChild(fragment)
 
-    
+
 }
 
 
